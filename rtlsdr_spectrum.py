a17d376458cd168571dbb4de153a0f8e41a2b575
Alexx <57861678+weider938@users.noreply.github.com>
2020-09-10 16:48:51 +0300

    Update rtlsdr_spectrum.py

--- rtlsdr_spectrum.py
+++ rtlsdr_spectrum.py
@@ -71,6 +71,5 @@ timer.timeout.connect(update)
 timer.start(0)
 
 if __name__ == '__main__':
-    import sys
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
         QtGui.QApplication.instance().exec_()
